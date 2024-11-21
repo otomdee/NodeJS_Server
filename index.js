52cb117546d9a6c0a6728b38973750fbf1c3ac9d
otomdee <otomdaniel65@gmail.com>
2024-11-21 10:42:40 +0100

    add routing

--- index.js
+++ index.js
@@ -3,13 +3,34 @@ const path = require('node:path');
 const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'src', 'index.html'), (err, content) => {
-            if (err) throw err;
+
+    let filePath = path.join(
+        __dirname,
+        'src',
+        req.url === '/' ? 'index.html' : req.url + '.html'
+    )
+
+    console.log(filePath);
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if(err.code == 'ENOENT') {
+                fs.readFile(path.join(__dirname, 'src', '404.html'), (inErr, content) => {
+                    if (inErr) console.log(inErr.code);
+                    res.writeHead(200, {'Content-Type' : 'text/html'});
+                    res.end(content);
+                })
+            }
+            else {
+                res.writeHead(500, {'Content-Type' : 'text/html'});
+                res.end(`<h1>Server error: ${err.code}</h1>`);
+            }
+        }
+        else {
             res.writeHead(200, {'Content-Type' : 'text/html'});
             res.end(content);
-        })
-    }
+        }
+    })
 })
 
 const PORT = process.env.PORT || 8080;
